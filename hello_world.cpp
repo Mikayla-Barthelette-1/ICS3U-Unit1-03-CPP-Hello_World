2e7ba020c5cfd6b1e7a5fa649c424d5201b1d932
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-10 16:33:21 +0000

    added the Hello, World! program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Mikayla Barthelette All rights reserved
+//
+// Created by: Mikayla Barthelette
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+}
